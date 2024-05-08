54ef9d7fcf344530fdd85309ba0c67c64c5ec601
Paul <username@users.noreply.github.com>
2024-05-08 16:19:04 -0700

    update component streaming

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -4,27 +4,11 @@ import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react';
-import { RevenueChartSkeleton, CardSkeleton } from '@/app/ui/skeletons';
+import { RevenueChartSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
+import CardWrapper from '@/app/ui/dashboard/cards';
 
 export default async function Page() {
   const latestInvoices = await fetchLatestInvoices();
-  const {
-    numberOfCustomers,
-    numberOfInvoices,
-    totalPaidInvoices,
-    totalPendingInvoices,
-  } = await fetchCardData();
-  
-  const cards = [
-    { title: 'Collected', value: totalPaidInvoices, type: 'collected' },
-    { title: 'Pending', value: totalPaidInvoices, type: 'pending' },
-    { title: 'Total Invoices', value: numberOfInvoices, type: 'invoices' },
-    {
-      title: 'Total Customers',
-      value: numberOfCustomers,
-      type: 'customers',
-    },
-  ];
 
   return (
     <main>
@@ -32,11 +16,9 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        {cards.map(({ title, value, type }) => (
-          <Suspense key={title} fallback={<RevenueChartSkeleton />}>
-            <Card title={title} value={value} type={type} />
-          </Suspense>
-        ))}
+        <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
+        </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<RevenueChartSkeleton />}>
